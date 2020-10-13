6b7e3d6ef2f6de993102d02495c840c90606c295
Burak T <burakturkman@gmail.com>
2020-10-13 22:18:19 +0300

    a new breach created and new lines added

--- src/test/java/LocalMergeConflict.java
+++ src/test/java/LocalMergeConflict.java
@@ -1,5 +1,6 @@
 public class LocalMergeConflict {
     public static void main(String[] args) {
         System.out.println("added some awesome line in local master branch");
+        //adding second way of saying hello
     }
 }
