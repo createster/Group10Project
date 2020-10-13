436f970e72450f10f88abd9c301ea630eab7d869
Burak T <burakturkman@gmail.com>
2020-10-13 22:20:22 +0300

    new changes for master

--- src/test/java/LocalMergeConflict.java
+++ src/test/java/LocalMergeConflict.java
@@ -1,5 +1,6 @@
 public class LocalMergeConflict {
     public static void main(String[] args) {
         System.out.println("added some awesome line in local master branch");
+        //adding more stuff here that hello2 does not know about
     }
 }
