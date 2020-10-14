e8baf76322391547db9d586af0b8e5a71e368609
gokhanguz <67508424+gokhanguz@users.noreply.github.com>
2020-10-14 19:48:45 +0300

    practice 1

--- src/test/java/gokhan.java
+++ src/test/java/gokhan.java
@@ -1,5 +1,6 @@
 public class gokhan {
     public static void main(String[] args) {
+        System.out.println("added new text");
         System.out.println("We will make the to project");
     }
 }
